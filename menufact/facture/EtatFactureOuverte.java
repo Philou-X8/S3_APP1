0ffbac767a6743956049c8b92ce1e9102e4949f6
Youlian <youlian@live.ca>
2023-05-08 13:03:28 -0400

    Etat->Facture

---
+++ menufact/facture/EtatFactureOuverte.java
@@ -0,0 +1,47 @@
+package menufact.facture;
+
+import menufact.facture.exceptions.FactureException;
+import menufact.plats.PlatChoisi;
+
+public class EtatFactureOuverte implements EtatFacture
+{
+    private Facture bill;
+
+    public  EtatFactureOuverte(Facture montant)
+    {
+        bill = montant;
+    }
+    @Override
+    public boolean isState(EtatFacture state)
+    {
+        if (state == this)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void ajoutePlat(PlatChoisi p) throws FactureException
+    {
+        bill.addPlat(p);
+    }
+
+    @Override
+    public void ouvrir() throws FactureException
+    {
+        throw new FactureException("La facture à l'état ouverte ne peut pas être reouverte.");
+    }
+
+    @Override
+    public void payer() throws FactureException
+    {
+        bill.setEtat(new EtatFacturePayer(bill));
+    }
+
+    @Override
+    public void fermer() throws FactureException
+    {
+        bill.setEtat(new EtatFactureFermer(bill));
+    }
+}
---
+++ menufact/facture/EtatFacturePayer.java
@@ -0,0 +1,45 @@
+package menufact.facture;
+
+import menufact.facture.exceptions.FactureException;
+import menufact.plats.PlatChoisi;
+
+public class EtatFacturePayer implements EtatFacture
+{
+    private Facture bill;
+
+    public EtatFacturePayer(Facture montant)
+    {
+        bill =montant;
+    }
+    @Override
+    public boolean isState(EtatFacture state) {
+        if (state == this)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void ajoutePlat(PlatChoisi p) throws FactureException
+    {
+        throw new FactureException("La facture à l'état payer ne peut pas ajouter des plat.");
+    }
+
+    @Override
+    public void ouvrir() throws FactureException
+    {
+        throw new FactureException("Une facture payé ne peut pas être reouverte.");
+    }
+
+    @Override
+    public void payer() throws FactureException {
+        throw new FactureException("La facture à l'état payer ne peux pas être payé à nouveau ");
+    }
+
+    @Override
+    public void fermer() throws FactureException
+    {
+        throw new FactureException("La facture à l'état payer ne peux pas être fermer ");
+    }
+}
